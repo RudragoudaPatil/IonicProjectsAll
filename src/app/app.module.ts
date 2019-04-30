a3540e479b6b8a03301d71a47d270b52f2d1b3a2
Rudragouda Patil <patil.rudragouda2@gmail.com>
2019-04-30 15:03:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,34 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { Firebase } from '@ionic-native/firebase/ngx'
+
+
+const config = {
+  apiKey: "AIzaSyCF1_lfi2fwFW1A8R08zV08sWsUzpEAQ9E",
+  authDomain: "fir-demo-538be.firebaseapp.com",
+  databaseURL: "https://fir-demo-538be.firebaseio.com",
+  projectId: "fir-demo-538be",
+  storageBucket: "fir-demo-538be.appspot.com",
+  messagingSenderId: "1033693710840"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
     IonicModule.forRoot(),
     AppRoutingModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Firebase,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
